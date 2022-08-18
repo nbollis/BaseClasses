277a4d38cca1fdea51dba24e52473fc69f1d17a5
Nic Bollis <nbollis@comcast.net>
2022-08-18 09:26:47 -0500

    Added all base classes

---
+++ MVVMBase/ValueConverters/Specific Use/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace MVVMBase
+{
+    public class BooleanToVisibilityConverter : BaseValueConverter<BooleanToVisibilityConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Boolean && (bool)value)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility && (Visibility)value == Visibility.Visible)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MVVMBase/ValueConverters/Specific Use/InverseBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MVVMBase
+{
+    public class InverseBooleanConverter : BaseValueConverter<InverseBooleanConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+    }
+}
